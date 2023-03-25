6b3338c4e348290ff33d907939f17cad69208121
David Gilinson <gilinson@fb.com>
2023-03-25 13:51:59 -0400

    updates to chatbot, filtering some responses

--- open_ai.py
+++ open_ai.py
@@ -8,7 +8,7 @@ prompt = "Write a funny and engaging tweet, that will get retweeted that is info
          "Include a link to the FEC filing here {link}." \
          "Do not indicate support for the donation or any cause, but have a general sentiment that money in politics is bad"
 
-fix_link_prompt = "Make sure this tweet: '{tweet}' has a link to an FEC filing. If it doesn't add this link: {link}."
+fix_link_prompt = "Make sure this tweet in quotes '{tweet}' has a link in it: {link}."
 
 shorten_prompt = "Shorten this to less than 280 characters: {input_text}."
 
@@ -40,6 +40,11 @@ def generate_tweet(input_text, link):
     tweet = re.sub(r'^"|"$', '', tweet) # remove quotes
     tweet = re.sub(r'[^\S\r\n]+', ' ', tweet) # drop whitespace
 
+    ai_pattern = re.compile(r'AI\s+language\s+model', re.IGNORECASE)
+    match = ai_pattern.search(tweet)
+    print(tweet)
+    if match:
+        tweet = None
     return tweet
 
 
@@ -54,4 +59,5 @@ def shorten_tweet(input_text):
     tweet = res.choices[0].message.content
     tweet = re.sub(r'^"|"$', '', tweet)  # remove quotes
     tweet = re.sub(r'[^\S\r\n]+', ' ', tweet)  # drop whitespace
+    print(tweet)
     return tweet
