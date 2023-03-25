5f29642b407a7345c69d9e69d962d8fd31620799
David Gilinson <gilinson@fb.com>
2023-03-25 14:31:36 -0400

    remove print statements, change prompt

--- open_ai.py
+++ open_ai.py
@@ -4,7 +4,8 @@ from credentials import open_ai_key
 openai.api_key = open_ai_key
 openai.Model.list()
 
-prompt = "Write a funny and engaging tweet, that will get retweeted that is informative and not offensive about {input_text} " \
+prompt = "Write a funny and engaging tweet, from the perspective of a robot that hates money in " \
+         "politics which that is likely to get retweeted which is informative and not offensive about {input_text} " \
          "Include a link to the FEC filing here {link}." \
          "Do not indicate support for the donation or any cause, but have a general sentiment that money in politics is bad"
 
@@ -42,7 +43,6 @@ def generate_tweet(input_text, link):
 
     ai_pattern = re.compile(r'AI\s+language\s+model', re.IGNORECASE)
     match = ai_pattern.search(tweet)
-    print(tweet)
     if match:
         tweet = None
     return tweet
@@ -59,5 +59,4 @@ def shorten_tweet(input_text):
     tweet = res.choices[0].message.content
     tweet = re.sub(r'^"|"$', '', tweet)  # remove quotes
     tweet = re.sub(r'[^\S\r\n]+', ' ', tweet)  # drop whitespace
-    print(tweet)
     return tweet
